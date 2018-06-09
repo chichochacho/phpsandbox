3829c902f4920f473dcf9506562e4e222cc76f1b
chichochacho <shaggyfo@gmail.com>
2018-06-09 00:02:39 -0600

    added website

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>My Website</title>
+</head>
+<body>
+  <header>
+    <ul>
+      <il><a href="index.php">Home</a></il>
+      <il><a href="about.php">About</a></il>
+      <il><a href="contact.php">Contact</a></il>
+    </ul>
+  </header>
